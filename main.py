5d42613e73f48f9b8490306b66eed17eb7c509d6
EndoEU <sam.boffey04@gmail.com>
2022-01-08 13:00:45 +0000

    positions

--- main.py
+++ main.py
@@ -145,13 +145,38 @@ def create_grid(locked_pos = {}):
     return grid
 
 def convert_shape_format(shape):
-    pass
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+    
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+    
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
 
 def valid_space(shape, grid):
-    pass
+    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub]
+    
+    formatted = convert_shape_format(shape)
+    
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+            
+    return True
 
 def check_lost(positions):
-    pass
+    for pos in positions:
+        x,y = pos
+        if y < 1:
+            return True
+        
+    return False
 
 def get_shape():
     return Piece(5, 0, random.choice(shapes))
@@ -160,13 +185,13 @@ def draw_text_middle(text, size, color, surface):
     pass
    
 def draw_grid(surface, grid):
-    surface.blit(label, (top_left_x + play_height/2 - (label.get_width()/2), 50))
+    sx = top_left_x
+    sy = top_left_y
     
     for i in range(len(grid)):
-        for j in range(len(gird[i])):
-            pygame.draw.rect(surface, grid[i][j], (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size), 0)
-    
-    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 4)
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * block_size), (sx + play_width, sy + i * block_size))
+        for j in range(len(grid[i])):
+            pygame.draw.line(surface, (128, 128, 128), (sx + j * block_size, sy + i * block_size), (sx + j * block_size, sy + play_height))
     
 def clear_rows(grid, locked):
     pass
@@ -174,7 +199,7 @@ def clear_rows(grid, locked):
 def draw_next_shape(shape, surface):
     pass
 
-def draw_window(surface):
+def draw_window(surface, grid):
     surface.fill((0, 0, 0))
     
     pygame.font.init()
@@ -182,6 +207,10 @@ def draw_window(surface):
     label = font.render('Tetris', 1, (255, 255, 255))
     
     surface.blit(label, (top_left_x + play_height/2 - (label.get_width()/2), 50))
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j * block_size, top_left_y + i * block_size, block_size, block_size), 0)
     
     draw_grid(surface, grid)
     pygame.display.update()
@@ -196,8 +225,21 @@ def main(win):
     next_piece = get_shape()
     clock = pygame.time.Clock()
     fall_time = 0
+    fall_speed = 0.27
     
     while run:
+        grid = create_grid(locked_pos)
+        fall_time += clock.get_rawtime()
+        clock.tick()
+        
+        if fall_time/1000 > fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            
+            if not(valid_space(current_piece , grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -219,6 +261,18 @@ def main(win):
                     current_piece.rotation += 1
                     if not(valid_space(current_piece, grid)):
                         current_piece -= 1
+                        
+        shape_pos = convert_shape_format(current_piece)
+        
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                grid[y][x] = current_piece.color
+                
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_pos[p] = current_piece.color
         
         draw_window(win, grid)
 
